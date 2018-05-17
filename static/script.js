0d09b74e08ff9b19e1fb47b199368a9cbcce9385
Haochuan Liu <haochuan.liu@gmail.com>
2018-05-17 01:13:23 -0700

    fix for ios browser

--- static/script.js
+++ static/script.js
@@ -2,29 +2,36 @@
 // _haochuan<haochuan.liu@gmai.com>
 // at May 16, 2018
 //
+//
+
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
 
 const slider = document.getElementById('freqSlider');
 const label = document.getElementById('freqLabel');
 
-const audioTag = document.getElementById('audioTag');
+let isPlaying = false;
+let audioBuffer = null; // store audio buffer
 // create audio source in web audio api
-const sourceNode = audioContext.createMediaElementSource(audioTag);
+// due to the support in iOS, cannot use createMediaElementSouce
+// const audioTag = document.getElementById('audioTag');
+// const sourceNode = audioContext.createMediaElementSource(audioTag);
 const gainNode = audioContext.createGain();
-gainNode.gain.value = 10;
+gainNode.gain.value = 15;
 
 const filterNode = audioContext.createBiquadFilter();
+let sourceNode;
 
 filterNode.type = 'bandpass'; // bandpass filter
 // filterNode.frequency.value = 1000;
 filterNode.gain.value = 100;
-filterNode.Q.value = 20;
+filterNode.Q.value = 5;
+
+loadAudio('/yanny-laurel.wav');
 
 // connect nodes
-sourceNode.connect(filterNode);
-filterNode.connect(gainNode);
-gainNode.connect(audioContext.destination);
+gainNode.connect(filterNode);
+filterNode.connect(audioContext.destination);
 
 // setup slider for freq
 slider.addEventListener(
@@ -35,3 +42,44 @@ slider.addEventListener(
   },
   false
 );
+
+// toggle button
+const button = document.getElementById('toggleAudio');
+
+button.addEventListener(
+  'click',
+  e => {
+    if (isPlaying) {
+      sourceNode.stop();
+      isPlaying = false;
+      button.innerHTML = 'Play';
+    } else {
+      sourceNode = audioContext.createBufferSource(); // creates a sound source
+      sourceNode.buffer = audioBuffer;
+      sourceNode.loop = true;
+      sourceNode.connect(gainNode);
+      sourceNode.start();
+      isPlaying = true;
+      button.innerHTML = 'Pause';
+    }
+  },
+  false
+);
+
+function loadAudio(url) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+
+  // Decode asynchronously
+  request.onload = function() {
+    audioContext.decodeAudioData(
+      request.response,
+      function(buffer) {
+        audioBuffer = buffer; // tell the source which sound to play
+      },
+      null
+    );
+  };
+  request.send();
+}
